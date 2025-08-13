6e495531a15418a5c3895f855e30ad5c6a16bb86
sierrapablo <pablo@metrio.es>
2025-08-13 09:34:06 +0200

    📦 build: Add Astro Node adapter, Tailwind CSS, and Prettier dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import node from '@astrojs/node';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  prefetch: true,
+  vite: { plugins: [tailwindcss()] },
+  adapter: node({ mode: 'standalone' }),
+});
